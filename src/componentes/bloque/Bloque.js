4832f3b3fb08262f593c18daafe073966bdd9240
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2019-06-03 14:44:09 -0500

    Add grid to imgs to item bloque

--- src/componentes/bloque/Bloque.js
+++ src/componentes/bloque/Bloque.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import './Bloque.css';
+import GridImgs from '../grid-imgs/GridImgs';
 
 class Bloque extends Component {
-
-    rutaImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREpSofzVas-SmYs5WLZXU9Yk7Ohr3QHiptH1eQddrGU-DRNLab";
 
     render() {
         return (
             <div className="cont-card-bloque">
                 <Card className="card-bloque">
-                    <CardMedia
-                        className="card-bloque-img"
-                        image={ this.rutaImg }
-                        title="Paella dish"
-                    />
+                    <GridImgs />
 
                     <CardContent>
                         <Typography variant="body2" color="textPrimary" component="p">
---
+++ src/componentes/grid-imgs/GridImgs.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './GridImgs.css';
+
+class GridImgs extends Component {
+
+    rutaImg1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREpSofzVas-SmYs5WLZXU9Yk7Ohr3QHiptH1eQddrGU-DRNLab";
+    rutaImg2 = "https://i.ytimg.com/vi/frMXIq2UvYM/maxresdefault.jpg";
+    rutaImg3 = "https://i.blogs.es/f0b8ef/01/450_1000.jpg";
+
+    render() {
+        return (
+
+            <div className="card-bloque-img">
+
+                <img className="img1" src={ this.rutaImg1 }/>
+                <img className="img2" src={ this.rutaImg2 }/>
+                <img className="img3" src={ this.rutaImg3 }/>
+
+            </div>
+        );
+    }
+
+}
+
+export default GridImgs;
